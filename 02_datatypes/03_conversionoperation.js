26e294bf61ab57bdd9af2746ffb5bffc7ee22e7a
Rohan Rajshekhar Konde <rohankonde.official@gmail.com>
2026-05-14 07:15:17 +0000

    learning

---
+++ 02_datatypes/03_conversionoperation.js
@@ -0,0 +1,13 @@
+let score="33";
+console.log(typeof score);
+
+let valueInNumber= Number(score);
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+let someNumber=30;
+let stringNumber=String(someNumber);
+console.log(typeof someNumber);
+console.log(typeof stringNumber);
+
---
+++ 02_datatypes/test.js
@@ -0,0 +1,10 @@
+let age=25;
+console.log(age);
+
+let name = "Rohan";
+
+console.log(`Hello ${name}`);
+let name1 = "Ganesh";
+console.log("Hi "+name1);
+
+
